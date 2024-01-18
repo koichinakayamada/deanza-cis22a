a2a80ef581894e97cfeef1cb7c15885a1390f733
koichi-nkymd <151496024+koichi-nkymd@users.noreply.github.com>
2024-01-17 16:54:11 -0800

    Add files via upload

---
+++ PrintBasicShapes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    // Triangle
+    std::cout << "    ^" << std::endl;
+    std::cout << "   ^^^" << std::endl;
+    std::cout << "  ^^^^^" << std::endl;
+    std::cout << " ^^^^^^^" << std::endl;
+    std::cout << "^^^^^^^^^" << std::endl;
+    
+    // Diamond
+    std::cout << "    &" << std::endl;
+    std::cout << "   & &" << std::endl;
+    std::cout << "  &   &" << std::endl;
+    std::cout << "   & &" << std::endl;
+    std::cout << "    &" << std::endl;
+
+    // Rectangle
+    std::cout << "**********" << std::endl;
+    std::cout << "*        *" << std::endl;
+    std::cout << "*        *" << std::endl;
+    std::cout << "*        *" << std::endl;
+    std::cout << "**********" << std::endl;
+
+    return 0;
+}
+
