da7f97be022ebb640386e2873418d73a62b38be9
Koichi Nakayamada <151496024+koichi-nkymd@users.noreply.github.com>
2024-02-17 18:23:23 -0800

    Add files via upload

---
+++ practice/3-1_preAndPostIncrements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int i = 5;
+    // int m = 5; // declare
+
+    // // Pre-increment
+    // int a = i++;
+    // cout << "i: " << i << endl;
+    // cout << "a: " << a << endl;
+
+    // // Post-increment
+    // int b = ++m;
+    // cout << "m: " << m << endl;
+    // cout << "b: " << b << endl;
+
+    int value;
+
+    int i = 1, j;
+    j = i++;
+    cout << i << " " << j << endl;
+
+    return 0;
+}
