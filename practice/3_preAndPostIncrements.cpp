b9e5885d713144d9a035e831c3cbd74948aae89a
Koichi Nakayamada <151496024+koichi-nkymd@users.noreply.github.com>
2024-02-17 22:00:59 -0800

    Update 3_preAndPostIncrements.cpp

--- practice/3_preAndPostIncrements.cpp
+++ practice/3_preAndPostIncrements.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-    // int i = 5;
-    // int m = 5; // declare
+    int i = 5;
+    int m = 5; // declare
 
-    // // Pre-increment
-    // int a = i++;
-    // cout << "i: " << i << endl;
-    // cout << "a: " << a << endl;
+    // Pre-increment
+    int a = i++;
+    cout << "i: " << i << endl;
+    cout << "a: " << a << endl;
 
-    // // Post-increment
-    // int b = ++m;
-    // cout << "m: " << m << endl;
-    // cout << "b: " << b << endl;
+    // Post-increment
+    int b = ++m;
+    cout << "m: " << m << endl;
+    cout << "b: " << b << endl;
 
     int value;
 
