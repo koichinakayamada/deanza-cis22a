5a0815d47152bc961271aae82007e12302751192
Koichi Nakayamada <151496024+koichi-nkymd@users.noreply.github.com>
2024-03-05 09:59:01 -0800

    Add files via upload

---
+++ practice/learningArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int const ARRAY_SIZE = 5;
+    // int numbers[ARRAY_SIZE];
+
+    // for (int count = 0; count < ARRAY_SIZE; count++){
+    //     numbers[count] = 99;
+    // }
+
+    // const int SIZE = 5;
+    // int scores[] = {79, 82, 3, 44, 4};
+    // int scores[SIZE] = {}; // Initialize all to 0
+
+    // string dayOfTheWeek[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    // cout << dayOfTheWeek << endl;
+
+    char name[] = "Alexa";
+    cout << name << endl;
+}
